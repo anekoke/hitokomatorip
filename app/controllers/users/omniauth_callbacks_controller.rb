f8dd2e46f85557943a945c7184e8e324a0873ccf
aneko <maildesuyo@anekoke.com>
2018-06-04 16:01:21 +0000

    twitter login

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def twitter
+    callback_from :twitter
+  end
+  
+  private
+  
+  def callback_from(provider)
+    provider = provider.to_s
+    
+    @user = User.find_for_oauth(request.env['omniauth.auth'])
+    
+    if @user.persisted?
+      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
+      sign_in_and_redirect @user, :event => :authentication
+    else 
+      flash[:danger] = I18n.t('devise.omniauth_callbacks.alert', kind: provider.capitalize)
+      session["devise.#{provider}_data"] = request.env['omniauth.auth'].except("extra")
+      redirect_to root_url
+    end
+  end
+end
+
