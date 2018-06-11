4c9154fad14ebd941da4f578583cef61e67c1f3b
aneko <maildesuyo@anekoke.com>
2018-06-11 14:06:53 +0000

    correct uniq error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,19 +7,19 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user
-    @frames = @user.frames.order('created_at DESC').page(params[:page]).per(8)
+    @frames = @user.frames.order('created_at DESC').distinct.page(params[:page]).per(6)
     count(@user)
   end
   
   def interest
     @user = current_user
-    @interest = @user.interest_frames.order('updated_at DESC').page(params[:page]).per(8)
+    @interest = @user.interest_frames.order('updated_at DESC').page(params[:page]).per(6)
     count(@user)
   end
   
   def visit
     @user = current_user
-    @visit = @user.visit_frames.order('updated_at DESC').page(params[:page]).per(8)
+    @visit = @user.visit_frames.order('updated_at DESC').page(params[:page]).per(6)
     count(@user)
   end
 
